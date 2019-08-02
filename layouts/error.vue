39b90ee580a399386e01bfdfa372a5129f8a9d5a
Donaël Walt <w.dodo.a@hotmail.fr>
2019-08-02 11:39:43 +0200

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+    head() {
+      return {
+        title: 'Error | INGENIVM PORTFOLIO'
+      }
+    },
+}
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    flex-direction: column;
+}
+
+h1{
+    font-size: 100px;
+    margin-bottom: 20px;
+    color: transparent;
+    -webkit-text-stroke: 2px white;
+}
+
+a{
+    color:white;
+    text-decoration: none;
+    background-color: darkslategrey;
+    padding: 10px;
+    border-radius: 8px;
+}
+</style>
