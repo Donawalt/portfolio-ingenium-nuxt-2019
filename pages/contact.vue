d7ca43c45c722a0453f105103ca3af90b92774aa
Donaël Walt <w.dodo.a@hotmail.fr>
2019-08-20 00:04:32 +0200

    contact form and update page

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,9 +4,23 @@
       <p class="we">
         <span ref="a">CONTACT</span>
       </p>
-      <p class="a">
-        <a ref="b" href="mailto:ingenium2100@gmail.com">ingenium2100@gmail.com</a>
-      </p>
+      <div class="form-container">
+        <form name="contact" action="https://app.99inbound.com/api/e/6dzYzcoN" method="post">
+        <label class="form-label" for="name">
+          Name:
+        </label>
+        <input class="form-field" name="name" id="name" />
+        <label class="form-label" for="email">
+          Email:
+        </label>
+        <input class="form-field" name="email" id="email" />
+        <label class="form-label" for="message">
+          Message:
+        </label>
+        <textarea class="form-field" name="message" id="message"></textarea>
+        <input class="form-button" type="submit" value="Send message" />
+      </form>
+      </div>
     </section>
   </div>
 </template>
@@ -86,4 +100,52 @@
     text-decoration: none;
   }
 
+  form{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 600px;
+    margin-top: 10px;
+  }
+
+  .form-container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+  .form-field{
+    min-height: 50px;
+    margin-bottom: 10px;
+    border-radius: 10px;
+    border: 1.5px solid white;
+    padding-left: 20px;
+    background-color: black;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: grey;
+    transition: 500ms;
+  }
+
+  .form-field:focus{
+    background-color: white;
+    transition: 500ms;
+    color: black;
+  }
+
+  .form-field[name=message]{
+    padding-top: 20px;
+    padding-right: 20px;
+  }
+  input[type=submit]{
+    height: 50px;
+    background-color: darkslategrey;
+    color: white;
+    border: none;
+    align-self: center;
+    width:50%;
+    max-width: 200px;
+    border-radius: 25px;
+    text-transform: uppercase;
+    font-weight: bold; 
+  }
 </style>
---
+++ pages/inbox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1>thanks for your message</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+<style scoped>
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    flex-direction: column;
+}
+
+h1{
+    font-size: 80px;
+    margin-bottom: 20px;
+    color: transparent;
+    -webkit-text-stroke: 2px white;
+}
+
+a{
+    color:white;
+    text-decoration: none;
+    background-color: darkslategrey;
+    padding: 10px;
+    border-radius: 8px;
+}
+</style>
